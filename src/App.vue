6bfa9c831563fd5d9a4bfca1e8c7a7cd94dcb378
aissa <aissa.amina.2014@gmail.com>
2023-06-22 17:21:58 +0200

    second  commit

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Accueil</RouterLink>
+        <RouterLink to="/about">Se Connecter</RouterLink>
       </nav>
     </div>
   </header>
